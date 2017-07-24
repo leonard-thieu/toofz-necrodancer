b111b6a18b67fff8b73060855384c28e62bf760a
Leonard Thieu <leonard.thieu@gmail.com>
2017-07-24 10:08:40 -0400

    [api] Use CDN versions of libraries exclusively.

--- toofz.NecroDancer.Web.Api/App_Start/BundleConfig.cs
+++ toofz.NecroDancer.Web.Api/App_Start/BundleConfig.cs
@@ -14,13 +14,6 @@ namespace toofz.NecroDancer.Web.Api
 
             Scripts.DefaultTagFormat = "<script src=\"{0}\" crossorigin=\"anonymous\"></script>";
 #endif
-            bundles.Add(new StyleBundle("~/css/bootstrap", "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/slate/bootstrap.min.css").Include(
-                "~/Content/bootstrap.css"));
-
-            bundles.Add(new ScriptBundle("~/js/jquery", "https://code.jquery.com/jquery-3.1.1.min.js").Include(
-                "~/Scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/js/bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js").Include(
-                "~/Scripts/bootstrap.js"));
         }
     }
 }
