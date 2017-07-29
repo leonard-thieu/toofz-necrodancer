c71fce5bf53d7b13b3c479e5b848e14d4161522c
Leonard Thieu <leonard.thieu@gmail.com>
2017-07-29 12:42:36 -0400

    Remove unused code.

--- toofz.NecroDancer.Leaderboards/EntityFramework/LeaderboardsContext.cs
+++ toofz.NecroDancer.Leaderboards/EntityFramework/LeaderboardsContext.cs
@@ -21,43 +21,19 @@ namespace toofz.NecroDancer.Leaderboards.EntityFramework
         }
 
         public virtual DbQuery<Leaderboard> Leaderboards => Set<Leaderboard>().AsNoTracking();
-        public virtual DbSet<DailyLeaderboard> DailyLeaderboards => Set<DailyLeaderboard>();
         public virtual DbQuery<Entry> Entries => Set<Entry>().AsNoTracking();
+        public virtual DbSet<DailyLeaderboard> DailyLeaderboards => Set<DailyLeaderboard>();
         public virtual DbQuery<DailyEntry> DailyEntries => Set<DailyEntry>().AsNoTracking();
         public virtual DbQuery<Player> Players => Set<Player>().AsNoTracking();
         public virtual DbQuery<Replay> Replays => Set<Replay>().AsNoTracking();
-
-        public virtual void SetModified<TEntity>(TEntity entity) where TEntity : class
-        {
-            Entry(entity).State = EntityState.Modified;
-        }
-
-        public DbRawSqlQuery<T> DailySeries<T>(DateTime date)
-        {
-            return Database.SqlQuery<T>(@"
-WITH DailySeries
-AS 
-(SELECT p.LeaderboardId,
-        LAG(p.Date) OVER (ORDER BY p.Date) AS Previous,
-        p.Date,
-        LEAD(p.Date) OVER (ORDER BY p.Date) AS Next
-FROM Leaderboards AS p)
-
-SELECT LeaderboardId,
-       Previous,
-       Date,
-       Next
-FROM DailySeries
-WHERE Date = @p0;", date);
-        }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             var configs = modelBuilder.Configurations;
 
             configs.Add(new LeaderboardConfiguration());
-            configs.Add(new DailyLeaderboardConfiguration());
             configs.Add(new EntryConfiguration());
+            configs.Add(new DailyLeaderboardConfiguration());
             configs.Add(new DailyEntryConfiguration());
             configs.Add(new PlayerConfiguration());
             configs.Add(new ReplayConfiguration());
