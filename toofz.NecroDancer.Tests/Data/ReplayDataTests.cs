216c75e6ff5430f3b239dd7e8ce84ccb4f544e0e
Leonard Thieu <leonard.thieu@gmail.com>
2017-07-29 16:30:49 -0400

    Add test for TryGetSeed.

---
+++ toofz.NecroDancer.Tests/Data/ReplayDataTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using toofz.NecroDancer.Replays;
+
+namespace toofz.NecroDancer.Tests.Data
+{
+    class ReplayDataTests
+    {
+        [TestClass]
+        public class TryGetSeed
+        {
+            [TestMethod]
+            public void Returns_Correct_Seed()
+            {
+                // Arrange
+                var replayData = new ReplayData();
+                var level = new LevelData
+                {
+                    Seed = 1580050689,
+                };
+                replayData.Levels.Add(level);
+
+                // Act
+                var success = replayData.TryGetSeed(out int seed);
+
+                // Assert
+                Assert.IsTrue(success);
+                Assert.AreEqual(603033, seed);
+            }
+        }
+    }
+}
