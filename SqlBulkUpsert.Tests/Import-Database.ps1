c05a9f9ffa1952cb3195dd7a486673c9246de99f
Leonard Thieu <leonard.thieu@gmail.com>
2017-07-31 16:20:12 -0400

    Fix file names.

--- SqlBulkUpsert.Tests/Import-Database.ps1
+++ SqlBulkUpsert.Tests/Import-Database.ps1
@@ -3,6 +3,6 @@ $sqlInstance = "(local)\SQL2014"
 $dbName = "SqlBulkUpsertTestDb"
 
 # attach mdf to local instance
-$mdfFile = join-path $startPath $dbName + ".mdf"
-$ldfFile = join-path $startPath $dbName + "_log.ldf"
+$mdfFile = join-path $startPath "SqlBulkUpsertTestDb.mdf"
+$ldfFile = join-path $startPath "SqlBulkUpsertTestDb_log.ldf"
 sqlcmd -S "$sqlInstance" -Q "Use [master]; CREATE DATABASE [$dbName] ON (FILENAME = '$mdfFile'),(FILENAME = '$ldfFile') for ATTACH"
