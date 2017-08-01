71c30bd20f18c88fb64fe83f02b9cc9d0c86dc63
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-01 11:12:15 -0400

    Remove GlobalAssemblyInfo.cs.

--- toofz.NecroDancer.Leaderboards/Properties/AssemblyInfo.cs
+++ toofz.NecroDancer.Leaderboards/Properties/AssemblyInfo.cs
@@ -1,6 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+[assembly: AssemblyCompany("toofz")]
+[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyTitle("toofz.NecroDancer.Leaderboards")]
 [assembly: AssemblyProduct("toofz.NecroDancer.Leaderboards")]
 [assembly: AssemblyVersion("1.1.*")]
