0425ef20f09fbf1413f168903f0130c587f3009f
Leonard Thieu <leonard-thieu@users.noreply.github.com>
2017-08-01 16:52:48 -0400

    Separate toofz.NecroDancer into its own repository (#15)
    * Reference toofz.NecroDancer as a NuGet package.
    
    * Remove toofz.NecroDancer-related configuration.

--- toofz.NecroDancer/Data/Shadow.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace toofz.NecroDancer.Data
-{
-    public sealed class Shadow
-    {
-        public string Path { get; set; }
-        public int OffsetX { get; set; }
-        public int OffsetY { get; set; }
-    }
-}
--- toofz.NecroDancer/Dungeons/Entity`1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace toofz.NecroDancer.Dungeons
-{
-    public abstract class Entity<T> : Entity
-        where T : Entity
-    {
-        public new T Clone() => (T)base.Clone();
-    }
-}
