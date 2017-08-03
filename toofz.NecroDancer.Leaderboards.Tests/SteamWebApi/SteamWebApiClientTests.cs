8c312115bf95570f11b7836f538edf952727f6d5
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-03 10:35:44 -0400

    Add tests for SteamWebApiClient.

---
+++ toofz.NecroDancer.Leaderboards.Tests/SteamWebApi/SteamWebApiClientTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RichardSzalay.MockHttp;
+using toofz.NecroDancer.Leaderboards.SteamWebApi;
+using toofz.NecroDancer.Leaderboards.SteamWebApi.ISteamUser;
+using toofz.NecroDancer.Leaderboards.Tests.Properties;
+
+namespace toofz.NecroDancer.Leaderboards.Tests.SteamWebApi
+{
+    class SteamWebApiClientTests
+    {
+        [TestClass]
+        public class GetPlayerSummariesAsync
+        {
+            [TestMethod]
+            public async Task SteamWebApiKeyIsNull_ThrowsInvalidOperationException()
+            {
+                // Arrange
+                var steamWebApiClient = new SteamWebApiClient(new MockHttpMessageHandler(), new LeaderboardsReader());
+
+                // Act
+                var ex = await Record.ExceptionAsync(() =>
+                {
+                    return steamWebApiClient.GetPlayerSummariesAsync(new long[0], null);
+                });
+
+                // Assert
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException));
+            }
+
+            [TestMethod]
+            public async Task steamIdsIsNull_ThrowsArgumentNullException()
+            {
+                // Arrange
+                var steamWebApiClient = new SteamWebApiClient(new MockHttpMessageHandler(), new LeaderboardsReader());
+                steamWebApiClient.SteamWebApiKey = "mySteamWebApiKey";
+
+                // Act
+                var ex = await Record.ExceptionAsync(() =>
+                {
+                    return steamWebApiClient.GetPlayerSummariesAsync(null, null);
+                });
+
+                // Assert
+                Assert.IsInstanceOfType(ex, typeof(ArgumentNullException));
+            }
+
+            [TestMethod]
+            public async Task TooManySteamIds_ThrowsArgumentException()
+            {
+                // Arrange
+                var steamWebApiClient = new SteamWebApiClient(new MockHttpMessageHandler(), new LeaderboardsReader());
+                steamWebApiClient.SteamWebApiKey = "mySteamWebApiKey";
+
+                // Act
+                var ex = await Record.ExceptionAsync(() =>
+                {
+                    return steamWebApiClient.GetPlayerSummariesAsync(new long[SteamWebApiClient.MaxPlayerSummariesPerRequest + 1], null);
+                });
+
+                // Assert
+                Assert.IsInstanceOfType(ex, typeof(ArgumentException));
+            }
+
+            [TestMethod]
+            public async Task ValidParams_ReturnsPlayerSummaries()
+            {
+                // Arrange
+                var handler = new MockHttpMessageHandler();
+                handler
+                    .When(HttpMethod.Get, "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=mySteamWebApiKey&steamids=76561197960435530")
+                    .RespondJson(Resources.GetPlayerSummaries);
+
+                var steamWebApiClient = new SteamWebApiClient(handler, new LeaderboardsReader());
+                steamWebApiClient.SteamWebApiKey = "mySteamWebApiKey";
+
+                // Act
+                var playerSummaries = await steamWebApiClient.GetPlayerSummariesAsync(new long[] { 76561197960435530 }, null);
+
+                // Assert
+                Assert.IsInstanceOfType(playerSummaries, typeof(PlayerSummaries));
+            }
+        }
+    }
+}
