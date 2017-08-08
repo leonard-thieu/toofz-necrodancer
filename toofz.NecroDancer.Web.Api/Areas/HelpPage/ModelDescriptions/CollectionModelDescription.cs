7944d21ea2c7ea8fc653d191f76e10f469f015a5
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-08 17:11:42 -0400

    Separate toofz.NecroDancer.Web.Api into its own repo.

--- toofz.NecroDancer.Web.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,8 +0,0 @@
-namespace toofz.NecroDancer.Web.Api.Areas.HelpPage.ModelDescriptions
-{
-    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- toofz.NecroDancer.Web.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace toofz.NecroDancer.Web.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
